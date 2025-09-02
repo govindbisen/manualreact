21c2130025be4a6e7f65fe245111d5562b61d721
unknown <govind.bisen@gmail.com>
2025-09-02 21:36:21 +0530

    sdfkjlsdjfl

---
+++ govind/Govind.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Govind() {
+	return <div>Govind Bisen</div>;
+}
+
+export default Govind;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Govind from '../govind/Govind';
+import Example from '../pb/Example';
+
+const App = () => {
+  return (
+    <>
+      <Example />
+      <Govind />
+    </>
+  );
+};
+
+export default App;
